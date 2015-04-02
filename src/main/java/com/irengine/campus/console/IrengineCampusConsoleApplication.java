3eb2374e03d79f3a1cf5e2bf1b471be8e0cd72c9
Alex Tang <alex.tang@taoware.com>
2015-04-02 16:50:09 +0800

    initial commit

---
+++ src/main/java/com/irengine/campus/console/IrengineCampusConsoleApplication.java
@@ -0,0 +1,12 @@
+package com.irengine.campus.console;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IrengineCampusConsoleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IrengineCampusConsoleApplication.class, args);
+    }
+}
---
+++ src/test/java/com/irengine/campus/console/IrengineCampusConsoleApplicationTests.java
@@ -0,0 +1,18 @@
+package com.irengine.campus.console;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = IrengineCampusConsoleApplication.class)
+@WebAppConfiguration
+public class IrengineCampusConsoleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
